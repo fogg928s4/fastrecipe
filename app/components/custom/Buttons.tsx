4ec11c06eed980d7e073408e0f4fe46f71aaa603
J Melg <rjmzelaya@gmail.com>
2025-11-12 23:59:45 +0100

    components workaround

--- app/components/custom/Buttons.tsx
+++ app/components/custom/Buttons.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useContext, useCallback} from 'react';
 import { LinksProps } from 'react-router-dom';
-import ButtonProps, {LinkButtonProps} from '../../../models/button.props';
+import ButtonProps, {LinkButtonProps} from '../../models/button.props';
 
 export default function LinkButton(props: LinkButtonProps) {
     return (
--- app/components/custom/NavBar.component.tsx
+++ app/components/custom/NavBar.component.tsx
@@ -1,1 +1,21 @@
 import React from "react";
+import LinkButton from "./Buttons";
+
+
+export default function NavBar() {
+    return 
+}
+
+export function NavBarFigure() {
+    return (
+        <figure>
+            <LinkButton
+                id="navbar-icon"
+                href="/index"
+                className="image"
+
+                 
+            />
+        </figure>
+    )
+}
